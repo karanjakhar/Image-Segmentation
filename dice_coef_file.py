69940dcd90cfa6d07ff3d6817abda71d5a9fe135
karanjakhar <karanjakhar49@gmail.com>
2019-08-29 01:04:51 +0530

    loss function

---
+++ dice_coef_file.py
@@ -0,0 +1,9 @@
+from keras import backend as K
+
+
+
+def dice_coef(y_true, y_pred, smooth=1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
